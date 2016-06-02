2ac6ce7ddf8911db1760ce9462f9d0ca9eb263ee
guangzhou-ljz <814930052@qq.com>
2016-06-02 10:29:29 +0800

    new zhangbeiXXX
    new zhangbeiXXX

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,33 +2,21 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-import {connect} from 'react-redux'
-import { login } from 'stores/modules/user';
 
 let yeomanImage = require('../images/yeoman.png');
 
-
-const mapStateToProps = (state) => ({
-  userProps: state.user
-});
-
-@connect(mapStateToProps)
-export default class Main extends React.Component {
-
-  componentDidMount() {
-  	const { dispatch } = this.props;
-  	dispatch(login());
-	}
-
+class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">mainPage</div>
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!’≈›ÌXXX</div>
       </div>
     );
   }
 }
 
-Main.propTypes = {
-}
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
